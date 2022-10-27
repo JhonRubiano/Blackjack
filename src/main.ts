e458132cfd4cfb86faae6b2db26ec21c0fbed390
JhonRubiano <rubianojhony@gmail.com>
2022-10-27 07:49:57 -0500

    refactor(module): modular ts and css files

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import './style.css'
 import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import { setupCounter } from './ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="./vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
