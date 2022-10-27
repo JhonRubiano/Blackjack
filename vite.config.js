03e8bbd0bc162cf27f8b430ad23588a1f60bad85
JhonRubiano <rubianojhony@gmail.com>
2022-10-27 08:05:26 -0500

    refactor(module):  assets build folder

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,18 @@ export default defineConfig({
     base:'/Blackjack/',
     build:{
         sourcemap: true,
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    let extType = assetInfo.name.split('.').at(1);
+                    if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+                        extType = 'images';
+                    }
+                    return `assets/${extType}/[name].[hash][extname]`;
+                },
+                chunkFileNames: 'assets/js/[name].[hash].js',
+                entryFileNames: 'assets/js/[name].[hash].js',
+            },
+        },
     }
 })
